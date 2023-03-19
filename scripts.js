4a4bb93da4fefb081502bf4861fdad13160c16cc
Hex <hexmusictheory@protonmail.com>
2023-03-19 03:13:34 +0000

    feat: added movement mechanics

--- scripts.js
+++ scripts.js
@@ -9,10 +9,16 @@ let context;
 let snakeX = blockSize * 5;
 let snakeY = blockSize * 5;
 
+let velocityX = 0;
+let velocityY = 0;
+
+let snakeBody = [];
+
 // snake food
 let foodX;
 let foodY;
 
+// initialises the game board / snake / food
 window.onload = function() {
     board = document.getElementById("board");
     board.height = rows * blockSize;
@@ -20,20 +26,51 @@ window.onload = function() {
     context = board.getContext("2d"); //used for drawing on the board
 
     placeFood();
-    update();
+    // change direction on key press
+    addEventListener("keydown", changeDirection);
+    setInterval(update, 1000/10);
 }
 
+// updates the board
 function update() {
+    // board stuff
     context.fillStyle = "black";
     context.fillRect(0, 0, board.width, board.height);
 
-    context.fillStyle = "lime";
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
-
+    // food stuff
     context.fillStyle = "red";
     context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    if (snakeX === foodX && snakeY === foodY) {
+        snakeBody.push(foodX, foodY);
+        placeFood();
+    }
+
+    // snake stuff
+    context.fillStyle = "lime";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
 }
 
+// changes the direction of the snake head on pressing a key
+function changeDirection(e) {
+    if (e.code === "ArrowUp" && velocityY !== 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.code === "ArrowDown" && velocityY !== -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.code === "ArrowLeft" && velocityX !== 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (e.code === "ArrowRight" && velocityX !== -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+// places food at a random coordinate on the canvas
 function placeFood() {
     foodX = Math.floor(Math.random() * cols) * blockSize;
     foodY = Math.floor(Math.random() * rows) * blockSize;
