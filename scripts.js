7400a767f653ba8e5276497c8dd67a23c97aed85
Hex <hexmusictheory@protonmail.com>
2023-03-19 02:38:23 +0000

    feat: add snake, and food objects

--- scripts.js
+++ scripts.js
@@ -4,3 +4,37 @@ let rows = 20;
 let cols = 20;
 let board;
 let context;
+
+// snake head
+let snakeX = blockSize * 5;
+let snakeY = blockSize * 5;
+
+// snake food
+let foodX;
+let foodY;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d"); //used for drawing on the board
+
+    placeFood();
+    update();
+}
+
+function update() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle = "lime";
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    context.fillStyle = "red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+}
